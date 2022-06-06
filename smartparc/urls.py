292a91bb9e8e3149d4ee00973a691ec93f60be9f
Amjed Bouralegue <Amjed.Bouralegue@bi-consulting.loc>
2022-06-06 23:04:59 +0100

    sever migrations fixed relations

--- smartparc/urls.py
+++ smartparc/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
 
 from personnel.views import UserViewset, CustomAuthToken, PermisViewset, PermisGetViewset, PasseportViewset, PasseportGetViewset, VisiteViewset, VisiteGetViewset
